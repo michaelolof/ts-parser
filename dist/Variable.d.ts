aa63411058c690714432fa8d5ff1b454d9b8652a
michaelolof <myqrrlolof@yahoo.com>
2018-05-08 18:36:59 +0100

    finished compiling @ Tue, May  8, 2018  6:36:59 PM

--- dist/Variable.d.ts
+++ dist/Variable.d.ts
@@ -1,6 +1,7 @@
 import { Range } from './utilities';
 import { VariableMember, Method } from './Member';
-import { VariableDeclaration, Symbol, SourceFile, Node } from 'typescript';
+import { VariableDeclaration, Symbol, SourceFile, Node, TypeChecker } from 'typescript';
+import { SymbolizedMemberArray } from './Checker';
 export declare class Variable {
     readonly element: VariableDeclaration;
     readonly filePath: string;
@@ -12,6 +13,7 @@ export declare class Variable {
     getMembers(): VariableMember[];
     getMethods(): Method[];
     getMembersSymbol(): Map<string, Symbol> | undefined;
+    getMembersSymbolizedMemberArray(checker: TypeChecker): Promise<SymbolizedMemberArray>;
     static IsAVariable(node: Node): node is VariableDeclaration;
     static Find(source: SourceFile): Promise<Variable[] | undefined>;
 }
